b1da9579b9a779fa8b71ab553eec27a13a11c2bc
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2022-11-09 15:28:24 -0500

    catch undefined lmod vars

--- compilers.sh
+++ compilers.sh
@@ -1,4 +1,7 @@
 function setcompilers () {
+    if [ -z "${TACC_FAMILY_COMPILER}" ] ; then \
+	echo "WARNING: variable TACC_FAMILY_COMPILER not set" ; \
+	fi 
     echo "Setting compilers for ${TACC_FAMILY_COMPILER}" >/dev/null \
      && if [ "${TACC_FAMILY_COMPILER}" = "intel" ] ; then \
         export cc=icc && export cxx=icpc && export fc=ifort \
@@ -6,8 +9,10 @@ function setcompilers () {
         export cc=icx && export cxx=icpx && export fc=ifx \
     ; elif [ "${TACC_FAMILY_COMPILER}" = "oneapi" ] ; then \
         export cc=icx && export cxx=icpx && export fc=ifx \
-    ; else \
+    ; elif [ "${TACC_FAMILY_COMPILER}" = "gcc" ] ; then \
         export cc=gcc && export cxx=g++ && export fc=gfortran \
+    ; else \
+        export cc=mpicc && export cxx=mpicxx && export fc=mpif90 \
     ; fi
 }
 
--- names.sh
+++ names.sh
@@ -1,5 +1,11 @@
 # -*- makefile -*-
 function setnames () {
+    if [ -z "${TACC_SYSTEM}" ] ; then \
+	echo "WARNING: variable TACC_SYSTEM not set" ; \
+	fi 
+    if [ -z "${TACC_FAMILY_COMPILER}" ] ; then \
+	echo "WARNING: variable TACC_FAMILY_COMPILER not set" ; \
+	fi 
     echo "Setting names for root=$1 package=$2 version=$3 ext=$4 basename=%5" >/dev/null \
 	&& export scriptdir=`pwd` \
 	&& PACKAGE=$2 \
