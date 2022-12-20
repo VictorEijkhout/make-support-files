355653b9fd95ac2a8cb91754d922b60c740e7873
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2022-12-19 23:56:45 -0600

    install path override for petsc

--- names.sh
+++ names.sh
@@ -32,10 +32,13 @@ function setnames () {
 	&& if [ ! -z "$4" -a ! "$4" = "keep" ] ; then \
 	       export installext=${installext}-$4 ; fi \
 	&& export builddir=${homedir}/build-${installext} \
-        && if [ -z "${INSTALLROOT}" ] ; then \
-	    export installdir=${homedir}/installation-${installext} \
+	&& if [ ! -z "${INSTALLPATH}" ] ; then \
+	     export installdir=${INSTALLPATH} \
 	   ; else \
-	    export installdir=${INSTALLROOT}/$package/installation-${installext} \
-	   ; fi \
+	     if [ -z "${INSTALLROOT}" ] ; then \
+	        export installdir=${homedir}/installation-${installext} \
+	     ; else \
+	        export installdir=${INSTALLROOT}/$package/installation-${installext} \
+	   ; fi ; fi \
         && export varfile=${scriptdir}/vars-${installext}.sh
 }
