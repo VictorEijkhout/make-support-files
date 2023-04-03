143b9d183a385ea45ff4491bff7dc9741d156bad
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-04-03 15:36:44 -0500

    add omp flag

--- compilers.sh
+++ compilers.sh
@@ -5,24 +5,32 @@ function setcompilers () {
     echo "Setting compilers for ${LMOD_FAMILY_COMPILER}" >/dev/null \
      && if [ "${LMOD_FAMILY_COMPILER}" = "intel" -o "${TACC_FAMILY_COMPILER}" = "intel" ] ; then \
         export cc=icc && export cxx=icpc && export fc=ifort \
+	export ompflag=-qopenmp \
     ; elif [ "${LMOD_FAMILY_COMPILER}" = "intelx" ] ; then \
         export cc=icx && export cxx=icpx && export fc=ifx \
-    ; elif [ "${LMOD_FAMILY_COMPILER}" = "clang" ] ; then \
-        export cc=clang && export cxx=clang++ && export fc=gfortran \
-    ; elif [ "${LMOD_FAMILY_COMPILER}" = "gcc" -o "${TACC_FAMILY_COMPILER}" = "gcc" ] ; then \
-        export cc=gcc && export cxx=g++ && export fc=gfortran \
+	export ompflag=-qopenmp \
     ; elif [ "${LMOD_FAMILY_COMPILER}" = "oneapi" -o "${TACC_FAMILY_COMPILER}" = "oneapi" ] ; then \
         export cc=icx && export cxx=icpx && export fc=ifx \
+	export ompflag=-qopenmp \
+    ; elif [ "${LMOD_FAMILY_COMPILER}" = "clang" ] ; then \
+        export cc=clang && export cxx=clang++ && export fc=gfortran \
+	export ompflag=-fopenmp \
+    ; elif [ "${LMOD_FAMILY_COMPILER}" = "gcc" -o "${TACC_FAMILY_COMPILER}" = "gcc" ] ; then \
+        export cc=gcc && export cxx=g++ && export fc=gfortran \
+	export ompflag=-fopenmp \
     ; else \
         export cc=mpicc && export cxx=mpicxx && export fc=mpif90 \
+	export ompflag=-fopenmp \
     ; fi \
     && export CC=$cc && export CXX=$cxx && export FC=$fc
 }
 
 function setmpicompilers () {
     echo "Setting MPI compilers for ${LMOD_FAMILY_COMPILER}" >/dev/null \
+     && setcompilers \
      && export cc=mpicc && export cxx=mpicxx && export fc=mpif90 \
-     && export CC=$cc && export CXX=$cxx && export FC=$fc
+     && export CC=$cc && export CXX=$cxx && export FC=$fc \
+
 }
 
 function reportcompilers () {
