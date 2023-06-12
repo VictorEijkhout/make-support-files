fb75ba3265a11aa653e3117be735a4c2ba93f0aa
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-06-12 11:19:13 -0500

    intel handling

--- compilers.sh
+++ compilers.sh
@@ -1,32 +1,41 @@
 function setcompilers () {
-    if [ -z "${LMOD_FAMILY_COMPILER}" -a -z "${TACC_FAMILY_COMPILER}" ] ; then \
+    if [ -z "${TACC_FAMILY_COMPILER}" ] ; then \
 	echo "WARNING: variable LMOD/TACC_FAMILY_COMPILER not set" ; \
 	fi 
-    echo "Setting compilers for ${LMOD_FAMILY_COMPILER}" >/dev/null \
+    echo "Setting compilers for ${TACC_FAMILY_COMPILER}" \
      && compiler_version=${TACC_FAMILY_COMPILER_VERSION} \
      && compiler_version=${compiler_version%%.*} \
-     && if [ "${LMOD_FAMILY_COMPILER}" = "intel" -o "${TACC_FAMILY_COMPILER}" = "intel" ] ; then \
-	if [ ${compiler_version} -lt 23 ] ; then \
-	    export cc=icc && export cxx=icpc && export fc=ifort \
+     && echo " .. version $${compiler_version}" \
+     && if [ "${TACC_FAMILY_COMPILER}" = "intel" ] ; then \
+	   if [ ${compiler_version} -lt 23 ] ; then \
+	       export cc=icc && export cxx=icpc && export fc=ifort \
+	       ; else \
+	       export cc=icx && export cxx=icpx && export fc=ifx \
+	       ; fi \
+	       && export ompflag=-qopenmp \
+	; elif [ "${TACC_FAMILY_COMPILER}" = "clang" ] ; then \
+	     if [ "${TACC_SYSTEM}" = "macbookair" ] ; then \
+		 export cc=clang-mp-16 \
+		     && export cxx=clang++-mp-16 \
+		     && export fc=gfortran \
+		 ; else \
+		 export cc=clang \
+		     && export cxx=clang++ \
+		     && export fc=gfortran \
+		 ; fi \
+		 && export ompflag=-fopenmp \
+	; elif [ "${TACC_FAMILY_COMPILER}" = "gcc" ] ; then \
+             export cc=gcc && export cxx=g++ && export fc=gfortran \
+		 && export ompflag=-fopenmp \
 	; else \
-	    export cc=icx && export cxx=icpx && export fc=ifx \
-	; fi \
-     && export ompflag=-qopenmp \
-    ; elif [ "${LMOD_FAMILY_COMPILER}" = "clang" ] ; then \
-        export cc=clang && export cxx=clang++ && export fc=gfortran \
-	export ompflag=-fopenmp \
-    ; elif [ "${LMOD_FAMILY_COMPILER}" = "gcc" -o "${TACC_FAMILY_COMPILER}" = "gcc" ] ; then \
-        export cc=gcc && export cxx=g++ && export fc=gfortran \
-	export ompflag=-fopenmp \
-    ; else \
-        export cc=mpicc && export cxx=mpicxx && export fc=mpif90 \
-	export ompflag=-fopenmp \
-    ; fi \
+             export cc=mpicc && export cxx=mpicxx && export fc=mpif90 \
+		 && export ompflag=-fopenmp \
+        ; fi \
     && export CC=$cc && export CXX=$cxx && export FC=$fc
 }
 
 function setmpicompilers () {
-    echo "Setting MPI compilers for ${LMOD_FAMILY_COMPILER}" >/dev/null \
+    echo "Setting MPI compilers for ${TACC_FAMILY_COMPILER}" >/dev/null \
      && setcompilers \
      && export cc=mpicc && export cxx=mpicxx && export fc=mpif90 \
      && export CC=$cc && export CXX=$cxx && export FC=$fc \
