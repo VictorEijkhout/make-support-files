13686cdb3167771545f582c4dc5c5c1c456685c0
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-14 06:44:57 -0500

    adding INSTALLVARIANT for extra level

--- names.sh
+++ names.sh
@@ -148,7 +148,10 @@ function setdirlognames() {
 	          export installdir=${installdir}-${package} \
 	        ; fi \
 	      && export installdir=${installdir}-${installext} \
-	   ; fi 
+	   ; fi \
+	 && if [ ! -z "${INSTALLVARIANT}" ] ; then \
+	        export installdir=${installdir}/${INSTALLVARIANT} \
+	    ; fi
 }
 
 function requirenonzero () {
