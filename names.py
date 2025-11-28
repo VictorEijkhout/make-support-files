8a18e07459a56d3f316b71bc3672ed2b5240e6cb
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-11-28 16:50:50 -0600

    build dir rule

--- names.py
+++ names.py
@@ -14,9 +14,10 @@ import process
 from process import echo_string,requirenonzero,nonnull
 # requirenonzeropath
 
-def packagenames( package,version,gitdate=None,terminal=sys.stdout ):
-    package = package.lower()
-    version = version.lower()
+def packagenames( **kwargs ):
+    package = kwargs.get("package").lower()
+    version = kwargs.get("packageversion").lower()
+    terminal = kwargs.get("terminal")
     if version == "git":
         raise Exception( "gitdate not yet implemented" )
     echo_string( f"setting internal variables packagebasename={package} packageversion={version}",
@@ -24,19 +25,27 @@ def packagenames( package,version,gitdate=None,terminal=sys.stdout ):
     return package,version
 
 def create_homedir( **kwargs ):
-    root     = kwargs.get("root",None)
-    package  = kwargs.get("package","nullpackage")
-    homedir  = kwargs.get("homedir",None )
+    root     = kwargs.get( "root",None )
+    package  = kwargs.get( "package","nullpackage" )
+    homedir  = kwargs.get( "homedir",None )
+    terminal = kwargs.get( "terminal",None )
     requirenonzero(root)
-    echo_string( f"computing homedir value based on root: {root}" )
-    package,_ = packagenames(package,"0,0")
-    if not homedir:
-        home = f"{root}/{package}"
-    echo_string( f"using homedir: {home}" )
-    if not os.path.isdir(home):
-        echo_string( f"creating homedir: {home}" )
-        os.mkdir(home)
-    return home
+    package,_ = packagenames(package=package,packageversion="0.0",termminal=terminal)
+    if root:
+        echo_string( f"creating homedir value based on root: {root}",terminal=terminal )
+        homedir = f"{root}/{package}"
+    else:
+        if not nonnull( homedir ): raise Exception( "need either root or homedir" )
+        echo_string( f"creating homedir value based on homedir: {homedir}",terminal=terminal )
+    echo_string( f"using homedir: {homedir}",terminal=terminal )
+    if not os.path.isdir(homedir):
+        echo_string( f"creating homedir: {homedir}",terminal=terminal )
+        try:
+            os.mkdir(homedir)
+        except PermissionError:
+            echo_string( f"ERROR: no permission to create homedir: {homedir}" )
+            sys.exit(1)
+    return homedir
 
 def compiler_names():
     compiler = os.environ['TACC_FAMILY_COMPILER']
@@ -45,21 +54,23 @@ def compiler_names():
     mpi = os.environ['TACC_FAMILY_MPI']
     mversion = os.environ['TACC_FAMILY_MPI_VERSION']
     return compiler,cversion,cshortv,mpi,mversion
+
 def environment_code( mode ):
     systemcode = os.environ['TACC_SYSTEM'] # systemnames
     compilercode,compilerversion,compilershortversion,mpicode,mpiversion = compiler_names()
     envcode = f"{systemcode}-{compilercode}{compilerversion}"
-    if mode=="mpi":
+    if mode in ["mpi","hybrid",]:
         envcode = f"{envcode}-{mpicode}{mpiversion}"
     return envcode
+
 def systemnames():
     compilercode,compilerversion,compilershortversion,mpicode,mpiversion = compiler_names()
     return mpicode,mpiversion
 
 def install_extension( **kwargs ):
-    package,packageversion = packagenames\
-        ( kwargs.get("package"),kwargs.get("packageversion"),
-          terminal=kwargs.get("terminal",None) )
+    package,packageversion = packagenames( **kwargs )
+        # ( kwargs.get("package"),kwargs.get("packageversion"),
+        #   terminal=kwargs.get("terminal",None) )
     envcode = environment_code( kwargs.get("mode") )
     installext = f"{packageversion}-{envcode}"
     if nonnull( iext := kwargs.get( "installext","" ) ):
@@ -68,13 +79,25 @@ def install_extension( **kwargs ):
         installext = f"{installext}-{variant}"
     return installext
 
+def builddir_name( **kwargs ):
+    package,packageversion = packagenames( **kwargs )
+        # ( kwargs.get("package"),kwargs.get("packageversion"),
+        #   terminal=kwargs.get("terminal",None) )
+    installext = install_extension( **kwargs )
+    if nonnull( bdir := kwargs.get("root","") ):
+        builddir = f"{bdir}/build-{installext}"
+    else:
+        homedir = create_homedir( **kwargs )
+        builddir = f"{homedir}/build-{installext}"
+    return builddir
+
 def prefixdir_name( **kwargs ):
-    prefixdir = ""
-    package,packageversion = packagenames\
-        ( kwargs.get("package"),kwargs.get("packageversion"),
-          terminal=kwargs.get("terminal",None) )
-    if nonnull( prefixdir:=kwargs.get("installpath","") ):
-        echo_string( f"Using external prefixdir: {prefixdir}" )
+    package,packageversion = packagenames( **kwargs )
+        # ( kwargs.get("package"),kwargs.get("packageversion"),
+        #   terminal=kwargs.get("terminal",None) )
+    if nonnull( pdir:=kwargs.get("installpath","") ):
+        echo_string( f"Using external prefixdir: {pdir}" )
+        prefixdir = pdir
     elif nonnull( kwargs.get("noinstall","") ):
         raise Exception( f"use of NOINSTALL not implemented" )
     else:
@@ -82,7 +105,7 @@ def prefixdir_name( **kwargs ):
         if nonnull( idir:=kwargs.get("installroot","") ):
             prefixdir = f"{idir}/installation"
         else: 
-            hdir = create_homedir( **kwargs );
+            hdir = create_homedir( **kwargs )
             prefixdir = f"{hdir}/installation"
         # attach package name
         if nonnull( mname:=kwargs.get("modulename","") ):
@@ -90,11 +113,11 @@ def prefixdir_name( **kwargs ):
         else:
             prefixdir = f"{prefixdir}-{package}"
         # install extension
-        installext = install_extension \
-            ( package=package,packageversion=packageversion,
-              installext=kwargs.get("installext",""),installvariant=kwargs.get("installvariant",""),
-              mode=kwargs.get("mode"),
-            )
+        installext = install_extension( **kwargs )
+            # ( package=package,packageversion=packageversion,
+            #   installext=kwargs.get("installext",""),installvariant=kwargs.get("installvariant",""),
+            #   mode=kwargs.get("mode"),
+            # )
         prefixdir = f"{prefixdir}-{installext}"
     if not nonnull( prefixdir ):
         raise Exception( "failed to set prefixdir" )
